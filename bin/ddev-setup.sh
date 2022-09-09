453921fe325bb15d449c683533225d6f830f53c0
Rodrigo Passos <rodrigopassos@mac.com>
2022-09-09 12:21:48 -0400

    add craft install step

---
+++ bin/ddev-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ask for the project name
+read -p "Project name (current)? " projectname
+
+echo [projectname] = $projectname
+
+# TODO: validate the project name
+
+# Genereta the config file
+echo "Generating creating DDEV config.yaml file"
+cp .ddev/template.config.yaml .ddev/config.yaml
+
+sed -i '' -e "s/\PROJECT_NAME/$projectname/g"  .ddev/config.yaml
+
+# Initialize DDEV
+echo "Copying ./cms/composer.ddev-installer.json to ./cms/composer.json"
+cp ./cms/composer.ddev-installer.json ./cms/composer.json
+ddev start
+
+# Install Craft
+echo "Installing Craft"
+ddev composer post-ddev-install
+
+# Clean up
