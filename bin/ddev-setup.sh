f9f43b04ccf541ef32d75cbcf31392d2f79133ed
Rodrigo Passos <rodrigopassos@mac.com>
2022-09-09 17:40:46 -0400

    add args and validation

--- bin/ddev-setup.sh
+++ bin/ddev-setup.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
+usage()
+{
+   # Display Help
+   echo "DDEV / CraftCMS setup script"
+   echo
+   echo "Syntax: ddev-setup.sh [-h|n|V]"
+   echo "options:"
+   echo "-h                 Print this Help."
+   echo "-n PROJECT_NAME    Initialize the project with the given name."
+   echo "-a                 Initialize the project with current directoy name."
+   echo
+}
+
 currentDir=${PWD##*/}
-# Ask for the project name
-echo -n "Project name [$currentDir]: "
-read project_name
+
+while getopts ":han:" option; do
+  case "${option}" in
+    h) # display Help
+      usage
+      exit;;
+
+    a) # Use current directory name as project name
+      project_name=$currentDir;;
+
+    n) # Enter project name
+      project_name=${OPTARG};;
+
+    *) # If unknown (any other) option:
+      usage
+      exit 1;;
+  esac
+done
 
 if [ "$project_name" == "" ];then
-   project_name=$currentDir
-else
-  project_name=`echo $project_name | tr '[:upper:]' '[:lower:]'`
-  project_name=`echo $project_name | tr -s '[:space:]' '-' | sed 's/.$//'`
+  echo -n "Project name [$currentDir]: "
+  read project_name
+  if [ "$project_name" == "" ];then
+    project_name=$currentDir
+  fi
 fi
+
+project_name=`echo $project_name | tr '[:upper:]' '[:lower:]'`
+project_name=`echo $project_name | tr -s '[:space:]' '-' | sed 's/.$//'`
 
 echo "Creating [$project_name] DDEV project"
 
